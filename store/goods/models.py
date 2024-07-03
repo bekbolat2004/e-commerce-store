ca0c0a329b0a64080a5161d98bcdf0ac2f8da0d1
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2024-07-03 13:28:14 +0500

    added db models, complete modified templates and worked with admin panel

--- store/goods/models.py
+++ store/goods/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00,max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    class Meta:
+       db_table = 'product'
+       verbose_name = 'Product'
+       verbose_name_plural = 'Products'
