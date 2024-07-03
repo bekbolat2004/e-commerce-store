6c9e9e40b3fc1a8dc1c1dcc880144c8079e7c5db
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2024-07-03 09:22:14 +0500

    created goods app, changed some js files, added html files for goods app

--- store/main/views.py
+++ store/main/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'title': 'Home - Главная',
-        'content': "Магазин мебели HOME"
+        'content': "Welcome to our online shop"
     }
     return render(request, 'main/index.html', context)
 
