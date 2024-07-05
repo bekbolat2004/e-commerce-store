559bda6ade4fb66202eaa7320a8afaa7f68ed8ca
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2024-07-05 21:08:43 +0500

    added library package for image field and configure settings

--- store/store/urls.py
+++ store/store/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from store import settings
+from store.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='goods'))
 ]
+
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
