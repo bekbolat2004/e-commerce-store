647ff67ef594ca2f72109f8cb6f6f700a4354b4c
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2024-07-14 17:45:02 +0500

    changed db to postgresql and implemented full search functionality

--- store/goods/utils.py
+++ store/goods/utils.py
@@ -1,4 +1,4 @@
-
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 from goods.models import Products
 from django.db.models import Q
 
@@ -6,13 +6,29 @@ def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    keywords = [word for word in query.split() if len(word) > 1]
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+    )
 
-    q_objects = Q()
+    result = (
+        result.annotate(headline= SearchHeadline("name", query, start_sel='<span style="background-color:yellow">', stop_sel="</span>"))
+    )
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    result = (
+        result.annotate(bodyline= SearchHeadline("description", query, start_sel='<span style="background-color:yellow">', stop_sel="</span>"))
+    )
 
-    return Products.objects.filter(q_objects)
+    return result
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    #
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
