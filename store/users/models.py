9ad78b4144b447cc7a22fbfe753f36fe6eceb177
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2024-07-20 21:10:59 +0500

    added forms file for login, registration and implemented login page but yet not completed

--- store/users/models.py
+++ store/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True)
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+        def __str__(self):
+            return self.username
+
+
--- store/users/views.py
+++ store/users/views.py
@@ -1,35 +1,49 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from users.forms import UserLoginForm
+
 
 # Creat 'e, our views here.
-def login(request) :
-
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
     context = {
-        'title': 'Home - Главная',
-
+        'title': 'Home - Authorization',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
-def regis(request) :
 
+def regis(request):
     context = {
         'title': 'Home - Главная',
 
     }
     return render(request, 'users/regis.html', context)
 
-def profile(request) :
 
+def profile(request):
     context = {
         'title': 'Home - Главная',
 
     }
     return render(request, 'users/profile', context)
 
-def logout(request) :
 
+def logout(request):
     context = {
         'title': 'Home - Главная',
 
     }
     return render(request, '', context)
-
