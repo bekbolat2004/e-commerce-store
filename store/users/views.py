4daede9f016493b90710783b9566789356325617
bekbolat2004 <bekbolatbekbolat32@gmail.com>
2024-08-18 18:52:27 +0500

    added profile form, simple notifications and ...

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,9 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
-
+from users.forms import UserLoginForm, UserRegistrationForm,ProfileForm
+from django.contrib.auth.decorators import login_required
 
 # Creat 'e, our views here.
 def login(request):
@@ -15,6 +15,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, you sucessfully logged in")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -42,16 +43,26 @@ def regis(request):
     return render(request, 'users/regis.html', context)
 
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request,f"Profile successfully updated")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         'title': 'Home - Главная',
-
+        'form': form
     }
-    return render(request, 'users/profile', context)
+    return render(request, 'users/profile.html', context)
 
-
+@login_required
 def logout(request):
-
+    messages.success(request,f"{request.user.username} you successfully logged out")
     auth.logout(request)
 
     return redirect(reverse('main:index'))
